70eec39f0c25f81511b69f0dfc80788186ef4599
Leo Prange <leonardoprange@gmail.com>
2017-10-10 22:18:18 -0300

    Criada soluções NET Core 2.0

--- ConsoleAppNetCore/Program.cs
+++ ConsoleAppNetCore/Program.cs
@@ -6,42 +6,7 @@ namespace ConsoleAppNetCore
     {
         static void Main(string[] args)
         {
-            int numero = 0, quantidadeDePrimosEncontrados = 0, quantidadeDePrimos;
-
-            Console.WriteLine("Digite a quantidade de primos que o sistema deve buscar: ");
-            quantidadeDePrimos = Convert.ToInt32(Console.ReadLine());
-            while (quantidadeDePrimosEncontrados != quantidadeDePrimos)
-            {
-                if (EhPrimo(numero))
-                    quantidadeDePrimosEncontrados++;
-                numero++;
-            }
-            Console.WriteLine("FIM");
+            Console.WriteLine("Hello World!");
         }
-        static bool EhPrimo(int numero)
-        {
-            int divisor;
-            bool ehPrimo;
-
-            if (ehPar(numero))
-                ehPrimo = false;
-            else
-                ehPrimo = true;
-
-            divisor = 3;
-            while (ehPrimo && divisor <= numero / 2)
-            {
-                if (numero % divisor == 0)
-                    ehPrimo = false;
-                divisor = divisor + 2;
-            }
-
-            if (ehPrimo)
-                return true;
-            else
-                return false;
-        }
-
-        static bool ehPar(int numero) => (numero <= 1 || (numero != 2 && numero % 2 == 0));
     }
 }
--- WebApiNetCore/Startup.cs
+++ WebApiNetCore/Startup.cs
@@ -7,44 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace WebApiNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
+            if (env.IsDevelopment())
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
         }
     }
 }
